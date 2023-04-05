383cb7f3c44e992a07db64ddbe5eb7aa1ae92f4d
sadambekov <bekavlad2005@gmail.com>
2023-04-05 15:20:26 +0600

    created a components folder and inside it created a layout component and added the necessary elements

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,17 @@
+import Nav from "../Nav/Nav";
+import "./Layout.css";
+
+export default function Layout(props) {
+  return (
+    <div className="Layout">
+      <header>
+        <Nav />
+      </header>
+      <aside>
+        <nav>NAV CATEGORIES</nav>
+      </aside>
+      <main>{props.children}</main>
+      <footer>FOOTER</footer>
+    </div>
+  );
+}
