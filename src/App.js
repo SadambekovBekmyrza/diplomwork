34c4d610f1b60d21f48c2164858b53b1deb42589
sadambekov <bekavlad2005@gmail.com>
2023-04-05 15:26:02 +0600

    added react router home and used it in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { Link, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>About</h1>} />
+          <Route path="/contacts" element={<h1>Contacts</h1>} />
+          <Route path="/delivery" element={<h1>Delivery</h1>} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
