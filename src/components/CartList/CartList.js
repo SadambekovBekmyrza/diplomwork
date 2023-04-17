52bceb9ecf26cae3a46dbd0b5c8b0a1fc2db2e27
sadambekov <bekavlad2005@gmail.com>
2023-04-17 15:38:20 +0600

    Added CartList components

---
+++ src/components/CartList/CartList.js
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import "./CartList.css";
+import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
+
+export default function CartList() {
+  const { products, cart, setCart } = useContext(AppContext);
+
+  const productIds = Object.keys(cart);
+
+  const output = products
+    .filter((product) => productIds.includes(product.id))
+    .map((product) => (
+      <div className="CartItem" key={product.id}>
+        <img src={product.picture} alt={product.name} />
+        <Link to={"/product/" + product.slug}>{product.name}</Link>
+        <input type="number" value={cart[product.id]} min={1} />
+        <span>{cart[product.id] * product.price} som</span>
+        <button>Remove</button>
+      </div>
+    ));
+  return <div className="CartList">{output}</div>;
+}
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,8 +9,10 @@ export default function Layout(props) {
     <div className="Layout">
       <header>
         <Nav />
-        <Logo></Logo>
-        <CartLink />
+        <div>
+          <Logo></Logo>
+          <CartLink />
+        </div>
       </header>
       <aside>
         <CategoryList />
