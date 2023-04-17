d1b6de0c86bee247bcf20ed84c5942284ef9e9af
sadambekov <bekavlad2005@gmail.com>
2023-04-17 15:57:20 +0600

    made working buttons in the CartList.js

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -6,6 +6,18 @@ import { Link } from "react-router-dom";
 export default function CartList() {
   const { products, cart, setCart } = useContext(AppContext);
 
+  function onQuantityChange(product, qty) {
+    setCart({
+      ...cart,
+      [product.id]: qty,
+    });
+  }
+  function onItemRemove(product) {
+    const newCart = { ...cart };
+    delete newCart[product.id];
+    setCart(newCart);
+  }
+
   const productIds = Object.keys(cart);
 
   const output = products
@@ -14,9 +26,14 @@ export default function CartList() {
       <div className="CartItem" key={product.id}>
         <img src={product.picture} alt={product.name} />
         <Link to={"/product/" + product.slug}>{product.name}</Link>
-        <input type="number" value={cart[product.id]} min={1} />
+        <input
+          type="number"
+          value={cart[product.id]}
+          min={1}
+          onChange={(event) => onQuantityChange(product, +event.target.value)}
+        />
         <span>{cart[product.id] * product.price} som</span>
-        <button>Remove</button>
+        <button onClick={() => onItemRemove(product)}>Remove</button>
       </div>
     ));
   return <div className="CartList">{output}</div>;
