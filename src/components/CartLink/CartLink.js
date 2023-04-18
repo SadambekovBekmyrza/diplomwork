27d57cc4d14787d5f516779d6f67e0c970347111
sadambekov <bekavlad2005@gmail.com>
2023-04-19 01:31:34 +0600

    Stylized CartList added element

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import "./CartLink.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
+import Logo from "../../assets/icon-cart.png";
 
 export default function CartLink() {
   const { cart } = useContext(AppContext);
@@ -10,7 +11,10 @@ export default function CartLink() {
 
   return (
     <div className="CartLink">
-      <NavLink to="/cart">Cart ({total})</NavLink>
+      <NavLink to="/cart">
+        <img src={Logo} alt="cart-logo" />
+        CART({total})
+      </NavLink>
     </div>
   );
 }
