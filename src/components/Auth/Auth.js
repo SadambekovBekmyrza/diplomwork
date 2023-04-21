0a65db21eb2bca66b474ea757ad4d2c199aceaed
sadambekov <bekavlad2005@gmail.com>
2023-04-22 02:07:15 +0600

    changed the header styled the navigation

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { signIn, signOff } from "../../firebase";
 import { AppContext } from "../../App";
+import "./Auth.css";
 
 export default function Auth() {
   const { user } = useContext(AppContext);
@@ -8,8 +9,8 @@ export default function Auth() {
   return (
     <div className="Auth">
       {!user ? <button onClick={signIn}>Sign in</button> : null}
+      {user ? <button onClick={signOff}>Sign out:</button> : null}
       {user ? <span>{user.displayName}</span> : null}
-      {user ? <button onClick={signOff}>Sign out</button> : null}
     </div>
   );
 }
