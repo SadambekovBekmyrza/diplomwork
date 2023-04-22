dd4b080f06dfe11c0d88697ed4adac83d7b0285e
sadambekov <bekavlad2005@gmail.com>
2023-04-22 16:37:54 +0600

    added products and styled

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -9,11 +9,13 @@ export default function ProductList({ category }) {
   const output = products
     .filter((product) => product.category === category.id)
     .map((product) => (
-      <div key={product.id} className="ProductList">
+      <div key={product.id} className="product">
         <img src={product.picture} alt={product.name} />
-        <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
-        <span>{product.price} som</span>
-        <AddToCart product={product} />
+        <div className="product-content">
+          <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
+          <span>{product.price} som</span>
+          <AddToCart product={product} />
+        </div>
       </div>
     ));
   return <div className="ProductList">{output}</div>;
