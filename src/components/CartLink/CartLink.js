b7ff0a185289898b909caa11f3827efe942dd29c
sadambekov <bekavlad2005@gmail.com>
2023-05-03 16:08:40 +0600

    added styling to cartlink.css and imported the desired image to cartlink.js

--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import "./CartLink.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
+import CartIcon from "../../assets/cart-icon.png";
 
 export default function CartLink() {
   const { cart } = useContext(AppContext);
@@ -11,8 +12,8 @@ export default function CartLink() {
   return (
     <div className="CartLink">
       <NavLink to="/cart">
-        <img src="https" alt="cart-logo" />
-        CART({total})
+        <img src={CartIcon} alt="cart-logo" />
+        <h6>CART({total})</h6>
       </NavLink>
     </div>
   );
