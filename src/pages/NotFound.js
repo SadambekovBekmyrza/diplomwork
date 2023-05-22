0a02ded4b56cadd61be1ce995c0a8dbe5eb76ef6
sadambekov <bekavlad2005@gmail.com>
2023-05-22 13:34:32 +0600

    Fixed bugs

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -2,9 +2,9 @@ import "./pages.css";
 export default function NotFound() {
   return (
     <div className="NotFound">
-      <div class="container">
-        <div class="row">
-          <div class="col-sm-12 col-md-12 mt-5 mb-5">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-12 mt-5 mb-5">
             <h2>NOT FOUND</h2>
             <h1>404</h1>
             <p>Oops! Something is wrong.</p>
